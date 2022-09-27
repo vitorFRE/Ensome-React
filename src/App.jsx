05ace85f4c79160fa2cf031e9e4a10d34ec481e7
Vitor <frete497@gmail.com>
2022-09-27 19:18:53 -0400

    Estrutura Inicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
