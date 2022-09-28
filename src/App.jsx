a4757f55e6e40b66613eda289438dbc55a8f97e8
Vitor <frete497@gmail.com>
2022-09-28 15:10:11 -0400

    Estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
